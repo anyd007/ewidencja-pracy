a2a3211658cd47209b590f3ea6bf18652923e386
Andrzej Gębala <andrzej.gebala35@gmail.com>
2026-05-19 17:44:46 +0200

    create pleace filter

--- src/pages/EmployeeDetails/EmployeeInfo.jsx
+++ src/pages/EmployeeDetails/EmployeeInfo.jsx
@@ -7,6 +7,7 @@ import "../../styles/EmployeeInfo.scss";
 const EmployeeInfo = ({ employee }) => {
   const [details, setDetails] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedWorkplace, setSelectedWorkplace] = useState("");
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
 
@@ -41,6 +42,21 @@ const EmployeeInfo = ({ employee }) => {
     return () => unsubscribe();
   }, [employee.id]);
 
+  
+  const workplaces = useMemo(() => {
+  const unique = new Map();
+
+  details.forEach((d) => {
+    unique.set(d.workplaceId, d.workplaceName);
+  });
+
+  return Array.from(unique, ([id, name]) => ({
+    id,
+    name,
+  }));
+}, [details]);
+
+
   // 🔥 filtr + sort (React)
   const filteredDetails = useMemo(() => {
     let result = [...details];
@@ -54,12 +70,17 @@ const EmployeeInfo = ({ employee }) => {
       result = result.filter((item) => item.date <= to);
     }
 
+    if (selectedWorkplace) {
+      result = result.filter(
+        (item) => item.workplaceId === selectedWorkplace);
+    }
+
     // sortowanie (najnowsze na górze)
     return result.sort((a, b) => new Date(b.date) - new Date(a.date));
-  }, [details, from, to]);
+  }, [details, from, to, selectedWorkplace]);
 
   const hasData = filteredDetails.length > 0;
-
+   console.log(selectedWorkplace);
   return (
     <div className="details">
       {loading && <Loader message="Pobieranie danych pracownika..." />}
@@ -71,17 +92,34 @@ const EmployeeInfo = ({ employee }) => {
       </h3>
 
       {/* FILTRY ZAWSZE WIDOCZNE */}
-      <div className="filters">
-        <input
-          type="date"
-          value={from}
-          onChange={(e) => setFrom(e.target.value)}
-        />
+      <div className="filters-wrapper">
+        <div className="filters-date">
+          <input
+            className="filters-input"
+            type="date"
+            value={from}
+            onChange={(e) => setFrom(e.target.value)}
+          />
 
-        <input 
-        type="date" 
-        value={to} 
-        onChange={(e) => setTo(e.target.value)} />
+          <input
+            className="filters-input"
+            type="date"
+            value={to}
+            onChange={(e) => setTo(e.target.value)}
+          />
+        </div>
+        <div className="filters-pleace">
+          <label>miejsce pracy</label><br />
+          <select
+            value={selectedWorkplace}
+            onChange={(e) => setSelectedWorkplace(e.target.value)}
+          >
+            <option value="">Wszytkie miejsca</option>
+            {workplaces.map((w) =>(
+                <option key={w.id} value={w.id}>{w.name}</option>
+            ))}
+          </select>
+        </div>
       </div>
 
       {/* STANY DANYCH */}
@@ -91,27 +129,27 @@ const EmployeeInfo = ({ employee }) => {
         <h3>Brak danych w wybranym zakresie</h3>
       ) : (
         <div className="table-wrapper">
-        <table>
-          <thead>
-            <tr>
-              <th>Miejsce pracy</th>
-              <th>Data</th>
-              <th>Start</th>
-              <th>Koniec</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {filteredDetails.map((detail) => (
-              <tr key={detail.id}>
-                <td>{detail.workplaceName}</td>
-                <td>{detail.date}</td>
-                <td>{detail.startTime}</td>
-                <td>{detail.endTime}</td>
+          <table>
+            <thead>
+              <tr>
+                <th>Miejsce pracy</th>
+                <th>Data</th>
+                <th>Start</th>
+                <th>Koniec</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+
+            <tbody>
+              {filteredDetails.map((detail) => (
+                <tr key={detail.id}>
+                  <td>{detail.workplaceName}</td>
+                  <td>{detail.date}</td>
+                  <td>{detail.startTime}</td>
+                  <td>{detail.endTime}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
